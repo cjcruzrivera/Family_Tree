620322bdf08795b70795feb93796bd72bca2996d
cjcruzrivera <cruz.camilo@correounivalle.edu.co>
2018-03-27 02:34:42 +0000

    Cambios para el servidor

--- Family_Tree/settings.py
+++ Family_Tree/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ubm^t6%)hw)lh8%z8um*cmzr@+x5^h1%h_%3&7_enw3wp@uzsk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,14 +76,18 @@ WSGI_APPLICATION = 'Family_Tree.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'family_tree',
-        'USER': 'ubuntu',
-        'PASSWORD': 'talentos',
-        'HOST': 'localhost',
-        'PORT': 5432,
-    }
+     'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'family_tree',
+#        'USER': 'ubuntu',
+#        'PASSWORD': 'talentos',
+#        'HOST': 'localhost',
+#        'PORT': 5432,
+#    }
 }
 
 
