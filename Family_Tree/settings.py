a0038e44204fc5af2c37553d3607981280c4cf59
cjcruzrivera <cruz.camilo@correounivalle.edu.co>
2018-03-27 03:04:55 -0500

    cambio server

--- Family_Tree/settings.py
+++ Family_Tree/settings.py
@@ -72,19 +72,19 @@ WSGI_APPLICATION = 'Family_Tree.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #        'ENGINE': 'django.db.backends.sqlite3',
-    #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #    }
-
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'family_tree',
-        'USER': 'ubuntu',
-        'PASSWORD': 'talentos',
-        'HOST': 'localhost',
-        'PORT': 5432,
-    }
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'family_tree',
+    #     'USER': 'ubuntu',
+    #     'PASSWORD': 'talentos',
+    #     'HOST': 'localhost',
+    #     'PORT': 5432,
+    # }
 }
 
 # Password validation
